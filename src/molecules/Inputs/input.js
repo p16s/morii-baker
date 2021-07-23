07b675518c6b8c8bfead84cb72719b6d6e70dbd0
Rob Dillon <smokinmesa@gmail.com>
2021-07-23 10:15:29 +0100

    .

--- src/molecules/Inputs/input.js
+++ src/molecules/Inputs/input.js
@@ -24,6 +24,7 @@ class Input extends BasicAtom {
                 >
                     {this.props.labelText}
                 </Label>
+
                 <FormInput
                     id={this.props.id}
                     value={this.props.value}
