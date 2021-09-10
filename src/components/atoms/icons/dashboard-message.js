5dca58b8658c3b7c65de8388f9fab6f3dac4b3a2
Rob Dillon <smokinmesa@gmail.com>
2021-09-10 11:06:36 +0100

    add scheduled tag

---
+++ src/components/atoms/icons/dashboard-message.js
@@ -0,0 +1,44 @@
+const IconDashboardMessage = () => {
+    return (
+        <svg className="icon-dashboard message" width="131" height="143" viewBox="0 0 131 143" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M131 54C131 66.1503 121.15 76 109 76C96.8497 76 87 66.1503 87 54C87 41.8497 96.8497 32 109 32C121.15 32 131 41.8497 131 54ZM92.5 54C92.5 63.1127 99.8873 70.5 109 70.5C118.113 70.5 125.5 63.1127 125.5 54C125.5 44.8873 118.113 37.5 109 37.5C99.8873 37.5 92.5 44.8873 92.5 54Z" fill="#BEF3E7"/>
+            <path d="M0 98L6 92L6 98L0 98Z" fill="#F3CEBA"/>
+            <path d="M26 7.15493e-08C25.2121 6.21533e-08 24.4319 0.155195 23.7039 0.456723C22.9759 0.758251 22.3145 1.20021 21.7574 1.75736C21.2002 2.31451 20.7583 2.97595 20.4567 3.7039C20.1552 4.43185 20 5.21207 20 6L26 6L26 7.15493e-08Z" fill="#F9BBD5"/>
+            <path d="M43 143H109C113.418 143 117 139.418 117 135V72.7653L76 33H43C38.5817 33 35 36.5817 35 41V135C35 139.418 38.5817 143 43 143Z" fill="#EDEBFF"/>
+            <g filter="url(#filter0_d)">
+                <path d="M34 133H101C105.418 133 109 129.418 109 125V63.4038L67.5 24H34C29.5817 24 26 27.5817 26 32V125C26 129.418 29.5817 133 34 133Z" fill="white"/>
+            </g>
+            <path d="M68 54.6021V24L109 63L75.9037 62.6015C71.5233 62.5488 68 58.9828 68 54.6021Z" fill="#FAFAFA"/>
+            <path d="M68 54.6021V24L109 63L75.9037 62.6015C71.5233 62.5488 68 58.9828 68 54.6021Z" fill="url(#paint0_linear)" fillOpacity="0.2"/>
+            <rect x="39" y="120" width="16" height="4" rx="2" fill="#4E32FB"/>
+            <rect x="61.25" y="120.25" width="15.5" height="3.5" rx="1.75" stroke="#725BFC" strokeWidth="0.5"/>
+            <rect x="83.25" y="120.25" width="15.5" height="3.5" rx="1.75" stroke="#725BFC" strokeWidth="0.5"/>
+            <line x1="39" y1="77.75" x2="94" y2="78.75" stroke="#F7F7F7" strokeWidth="0.5"/>
+            <rect x="39" y="68" width="35" height="4" rx="2" fill="#EDEBFF"/>
+            <rect x="39" y="84" width="30" height="4" rx="2" fill="#EDEBFF"/>
+            <rect x="39" y="91" width="54" height="4" rx="2" fill="#EDEBFF"/>
+            <rect x="39" y="98" width="48" height="4" rx="2" fill="#EDEBFF"/>
+            <rect x="39" y="105" width="48" height="4" rx="2" fill="#EDEBFF"/>
+            <defs>
+                <filter id="filter0_d" x="22" y="20" width="91" height="117" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                    <feOffset/>
+                    <feGaussianBlur stdDeviation="2"/>
+                    <feComposite in2="hardAlpha" operator="out"/>
+                    <feColorMatrix type="matrix" values="0 0 0 0 0.929412 0 0 0 0 0.921569 0 0 0 0 1 0 0 0 1 0"/>
+                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+                </filter>
+                <linearGradient id="paint0_linear" x1="99.2625" y1="33.2323" x2="67.9761" y2="63.469" gradientUnits="userSpaceOnUse">
+                    <stop stopColor="#CAC2FE"/>
+                    <stop offset="0.942708" stopColor="#F2F2F2"/>
+                    <stop offset="0.9999" stopColor="#F7F7F7" stopOpacity="0.265625"/>
+                </linearGradient>
+            </defs>
+        </svg>
+    );
+}
+
+
+export default IconDashboardMessage;
